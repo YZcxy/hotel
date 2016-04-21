8b0a4cea65645f594aa8b1729a96148e5ac2a437
HuChuan <691523638@qq.com>
2016-04-21 14:37:34 +0800

    room_po

---
+++ src/com/hotel/po/Room.java
@@ -0,0 +1,60 @@
+package com.hotel.po;
+
+public class Room {
+	int r_id;
+	int r_typeid;
+	String r_num;
+	String r_size;
+	String r_bed;
+	int r_isbath;
+	float r_money;
+	String r_img;
+	public int getR_id() {
+		return r_id;
+	}
+	public void setR_id(int r_id) {
+		this.r_id = r_id;
+	}
+	public int getR_typeid() {
+		return r_typeid;
+	}
+	public void setR_typeid(int r_typeid) {
+		this.r_typeid = r_typeid;
+	}
+	public String getR_num() {
+		return r_num;
+	}
+	public void setR_num(String r_num) {
+		this.r_num = r_num;
+	}
+	public String getR_size() {
+		return r_size;
+	}
+	public void setR_size(String r_size) {
+		this.r_size = r_size;
+	}
+	public String getR_bed() {
+		return r_bed;
+	}
+	public void setR_bed(String r_bed) {
+		this.r_bed = r_bed;
+	}
+	public int getR_isbath() {
+		return r_isbath;
+	}
+	public void setR_isbath(int r_isbath) {
+		this.r_isbath = r_isbath;
+	}
+	public float getR_money() {
+		return r_money;
+	}
+	public void setR_money(float r_money) {
+		this.r_money = r_money;
+	}
+	public String getR_img() {
+		return r_img;
+	}
+	public void setR_img(String r_img) {
+		this.r_img = r_img;
+	}	
+}
