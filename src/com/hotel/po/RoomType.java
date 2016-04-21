247d92192a93b54eae6a448d0c623a6f2095596c
HuChuan <691523638@qq.com>
2016-04-21 14:52:22 +0800

    roomType_po

---
+++ src/com/hotel/po/RoomType.java
@@ -0,0 +1,18 @@
+package com.hotel.po;
+
+public class RoomType {
+	int rt_id;
+	String rt_name;
+	public int getRt_id() {
+		return rt_id;
+	}
+	public void setRt_id(int rt_id) {
+		this.rt_id = rt_id;
+	}
+	public String getRt_name() {
+		return rt_name;
+	}
+	public void setRt_name(String rt_name) {
+		this.rt_name = rt_name;
+	}
+}
