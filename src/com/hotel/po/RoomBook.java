6d10494c90f08d3b964bc84d2d8590194d74b543
HuChuan <691523638@qq.com>
2016-04-22 19:53:44 +0800

    房间预定1

---
+++ src/com/hotel/po/RoomBook.java
@@ -0,0 +1,41 @@
+package com.hotel.po;
+
+import java.sql.Date;
+
+public class RoomBook {
+	private int rb_id;
+	private int rb_uid;
+	private int rb_rid;
+	private Date rb_bookdate;
+	private Date rb_indate;
+	public int getRb_id() {
+		return rb_id;
+	}
+	public void setRb_id(int rb_id) {
+		this.rb_id = rb_id;
+	}
+	public int getRb_uid() {
+		return rb_uid;
+	}
+	public void setRb_uid(int rb_uid) {
+		this.rb_uid = rb_uid;
+	}
+	public int getRb_rid() {
+		return rb_rid;
+	}
+	public void setRb_rid(int rb_rid) {
+		this.rb_rid = rb_rid;
+	}
+	public Date getRb_bookdate() {
+		return rb_bookdate;
+	}
+	public void setRb_bookdate(Date rb_bookdate) {
+		this.rb_bookdate = rb_bookdate;
+	}
+	public Date getRb_indate() {
+		return rb_indate;
+	}
+	public void setRb_indate(Date rb_indate) {
+		this.rb_indate = rb_indate;
+	}
+}
